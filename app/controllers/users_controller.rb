df580d6381d4d30997528d0b0f3325608e66ea21
Jack Chen <lalala.jack@gmail.com>
2015-07-26 15:30:16 +0800

    Add user#create in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'Signed Up!'
+      redirect_to groceries_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email,
+                                 :password,
+                                 :password_confirmation,
+                                 :name)
+  end
 end
