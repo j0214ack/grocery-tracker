f2ddb4c6196b34f086390d710ddab18f3bc888c7
Jack Chen <lalala.jack@gmail.com>
2015-07-26 15:15:08 +0800

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+
+  has_secure_password validations: false
+  
+  validates :email, presence: true, uniqueness: true
+  validates :password, on: :create, presence: true, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :name, presence: true
+end
