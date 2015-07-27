8d38875a8b1f1dea327b858b631cdf3119b36d0b
Jack Chen <lalala.jack@gmail.com>
2015-07-27 10:42:53 +0800

    Done login authentications

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,8 @@
 class SessionsController < ApplicationController
+
+  before_action :require_singed_out
+
   def new
-    if logged_in?
-      flash[:error] = "You've already signed in!"
-      redirect_to groceries_path
-    end
   end
 
   def create
@@ -11,6 +10,16 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       flash[:success] = "Welcome back, #{user.name}!"
       session[:user_id] = user.id
+      redirect_to groceries_path
+    else
+      flash[:error] = "Wrong email or password."
+      render :new
+    end
+  end
+
+  def require_singed_out
+    if logged_in?
+      flash[:error] = "You've already signed in!"
       redirect_to groceries_path
     end
   end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -17,27 +17,54 @@ describe SessionsController do
         expect(flash[:error]).to be_present
       end
     end
-
-    context 'when user not signed in' do
-      it 'render the sign in page'
-    end
   end
 
   describe 'POST create' do
     context 'when user already signed in' do
-      it "redirects to user's grocery list page"
-      it "shows a flash error message"
+      before do
+        login_user
+        post :create
+      end
+
+      it "redirects to user's grocery list page" do
+        expect(response).to redirect_to groceries_path
+      end
+
+      it "shows a flash error message" do
+        expect(flash[:error]).to be_present
+      end
     end
 
     context 'when provided with valid information' do
-      it "sets the session['user_id'] to user's id"
-      it "redirects to user's grocery list page"
-      it "shows a flash success message"
+      let(:user) { Fabricate(:user) }
+
+      it "sets the session[:user_id] to user's id" do
+        post :create, { email: user.email, password: 'password' }
+
+        expect(session[:user_id]).to eq user.id
+      end
+
+      it "redirects to user's grocery list page" do
+        post :create, { email: user.email, password: 'password' }
+        expect(response).to redirect_to groceries_path
+      end
+
+      it "shows a flash success message" do
+        post :create, { email: user.email, password: 'password' }
+        expect(flash[:success]).to be_present
+      end
     end
 
     context 'when provided with invalid information' do
-      it 'renders the sign in page'
-      it 'shows a flash error message'
+      it 'renders the sign in page' do
+        post :create
+        expect(response).to render_template :new
+      end
+
+      it 'shows a flash error message' do
+        post :create
+        expect(flash[:error]).to be_present
+      end
     end
   end
 end
